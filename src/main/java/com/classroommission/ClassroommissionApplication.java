9717485c601a97421bdac25cf21cb2da54ca9d98
MateusChitolina <mateus.ex.chitolina@gmail.com>
2024-09-18 08:28:15 -0300

    Configuration swagger

---
+++ src/main/java/com/classroommission/ClassroommissionApplication.java
@@ -0,0 +1,13 @@
+package com.classroommission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroommissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroommissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classroommission/ClassroommissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classroommission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroommissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
