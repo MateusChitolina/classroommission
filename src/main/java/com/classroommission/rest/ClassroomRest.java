3b5c4b50ae0793a827a830d3e306ec162cc6daae
MateusChitolina <mateus.ex.chitolina@gmail.com>
2024-09-29 21:39:45 -0300

    Adicionado para aceitar requisição de 3000

--- src/main/java/com/classroommission/rest/ClassroomRest.java
+++ src/main/java/com/classroommission/rest/ClassroomRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.classroommission.service.ClassroomService;
 
 @RestController
 @RequestMapping("/classroom")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ClassroomRest {
 
   private ClassroomService classroomService;
